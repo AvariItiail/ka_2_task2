d0bbba1febf345d9c2d72812ff1c60062daffde9
avariitigail <sashaokolova@gmail.com>
2019-10-21 15:28:10 +0500

    task 2

---
+++ ka_task_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ka_task_2
+{
+    class Program
+    {
+        private static readonly string inputFileName = @"in.txt";
+        private static readonly string outputFileName = @"out.txt";
+
+        static void Main()
+        {
+            File.Delete(outputFileName);
+            var input = File.ReadAllLines(inputFileName);
+            var graph = new List<int>[int.Parse(input[0])];
+            for (var node = 0; node < graph.Length; node++)
+            {
+                var line = input[node + 1].Trim().Split(' ');
+                var incidentNodesCount = line.Length - 1;
+                var incidentNodes = new List<int>(incidentNodesCount);
+                for (var i = 0; i < incidentNodesCount; i++)
+                    incidentNodes.Add(int.Parse(line[i]) - 1);
+                graph[node] = incidentNodes;
+            }
+
+            if (IsBipartite(graph, out var bipartiteNodes))
+            {
+                var output = new StringBuilder();
+                output.AppendLine("Y");
+
+                for (var node = 0; node < graph.Length; node++)
+                    if ((bool)bipartiteNodes[node])
+                        output.Append($"{node + 1} ");
+                output.AppendLine("0");
+
+                for (var node = 1; node < graph.Length; node++)
+                    if ((bool)!bipartiteNodes[node])
+                        output.Append($"{node + 1} ");
+                output.Append(0);
+                File.WriteAllText(outputFileName, output.ToString());
+            }
+            else
+                File.WriteAllText(outputFileName, "N");
+        }
+
+        private static bool IsBipartite(List<int>[] graph, out bool?[] parts)
+        {
+            parts = new bool?[graph.Length];
+            parts[0] = true;
+            var stack = new Stack<int>();
+            stack.Push(0);
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                foreach (var incidentNode in graph[node])
+                {
+                    if (parts[incidentNode] == parts[node]) return false;
+                    if (parts[incidentNode] == null)
+                    {
+                        parts[incidentNode] = !parts[node];
+                        stack.Push(incidentNode);
+                    }
+                }
+            }
+            return !parts.Contains(null);
+        }
+    }
+    
+}
